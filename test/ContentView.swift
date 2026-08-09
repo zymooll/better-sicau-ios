91c70c6d98c3698534b418f1a3e6653b4a359277
zymooll <zymooll@qq.com>
2026-08-10 00:36:45 +0800

    Initial Commit

---
+++ test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by zymooll on 2026/8/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by zymooll on 2026/8/10.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
